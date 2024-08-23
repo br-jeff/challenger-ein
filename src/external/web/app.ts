6cadf848390daa491e1a309cd3a38a24439c85b0
jefferson Silva <jefferson_1996Th@hotmail.com>
2024-08-22 21:55:08 -0300

    set tsyringe container

--- src/external/web/app.ts
+++ src/external/web/app.ts
@@ -1,9 +1,14 @@
-import { createExpressServer } from 'routing-controllers';
+import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { createExpressServer,useContainer  } from 'routing-controllers';
 import { UserController } from '@src/application/controllers/user-controller'
 import { HealthController } from '@src/application/controllers/health'
+import { TsyringeAdapter } from '@src/external/config/container';
 
 const app = createExpressServer({
   controllers: [UserController, HealthController],
 });
+
+useContainer(new TsyringeAdapter(container))
 
 app.listen(3000);

def59054d096dfd47140070b92f8c8a5b7566cfc
jefferson Silva <jefferson_1996Th@hotmail.com>
2024-08-22 21:17:06 -0300

    users and health imported

--- src/external/web/app.ts
+++ src/external/web/app.ts
@@ -1,10 +1,9 @@
 import { createExpressServer } from 'routing-controllers';
-import { UserController } from '@src/application/use-case/user-controller'
+import { UserController } from '@src/application/controllers/user-controller'
 import { HealthController } from '@src/application/controllers/health'
 
 const app = createExpressServer({
-  controllers: [UserController, HealthController], // we specify controllers we want to use
+  controllers: [UserController, HealthController],
 });
 
-// run express application on port 3000
 app.listen(3000);

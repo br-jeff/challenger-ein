a573afedccc04301adc1e4ac85e79f55d734c7bb
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-08-22 23:30:16 -0300

    set logger

--- src/external/web/app.ts
+++ src/external/web/app.ts
@@ -1,15 +1,21 @@
 import 'reflect-metadata';
 import { container } from 'tsyringe';
-import { createExpressServer,useContainer  } from 'routing-controllers';
+import { createExpressServer, useContainer } from 'routing-controllers';
 import { UserController } from '@src/application/controllers/user-controller'
 import { HealthController } from '@src/application/controllers/health'
 import { TsyringeAdapter } from '@src/external/config/container';
+import { logger } from '@src/external/utils/logger';
 import { bootstrap } from './bootstrap'
 
+const port = 3000
 const app = createExpressServer({
   controllers: bootstrap(),
 });
 
 useContainer(new TsyringeAdapter(container))
 
-app.listen(3000);
+app.on('error', (error: Error) => logger.error({ error }))
+
+app.listen(port, async () => {
+  logger.info(`App is running on port ${port}`);
+});
--- src/external/web/bootstrap.ts
+++ src/external/web/bootstrap.ts
@@ -1,7 +1,6 @@
 import fs from 'fs'
 import path from 'path'
 
-
 export function bootstrap() {
     const controllerPath = path.resolve(__dirname, '../../application/controllers');
     const controllers = fs.readdirSync(controllerPath).map((fileName) =>
